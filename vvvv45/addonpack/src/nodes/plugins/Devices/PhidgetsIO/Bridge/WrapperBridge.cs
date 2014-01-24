dd27f70c2050d15fda92f1f2c2205ae35953f5b0
Nils Buhlert <buhlert@meso.net>
2014-01-24 16:22:45 +0100

    add bridge files

---
+++ vvvv45/addonpack/src/nodes/plugins/Devices/PhidgetsIO/Bridge/WrapperBridge.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Phidgets;
+using Phidgets.Events;
+
+namespace VVVV.Nodes
+{
+    class WrapperBridge: Phidgets<Bridge>, IPhidgetsWrapper
+    {
+
+        bool FChanged = false;
+		bool FSensorChanged = false;
+		bool FDigitalInputChanged = false;
+
+        #region constructor
+
+        public WrapperBridge()
+            : base()
+        {
+
+        }
+
+        public WrapperBridge(int SerialNumber)
+            : base(SerialNumber)
+        {
+
+        }
+
+        #endregion constructor
+
+
+        #region setter fuctions
+
+
+        public void SetBridgeEnable(int Index, bool value)
+        {
+            FPhidget.bridges[Index].Enabled = value;
+        }
+
+        public void SetBridgeGain(int Index, BridgeInput.Gains value)
+        {
+            FPhidget.bridges[Index].Gain = value;
+        }
+
+
+        #endregion setter function
+
+
+        #region getter functions
+
+        public bool GetBridgeEnable(int Index, bool value)
+        {
+            return FPhidget.bridges[Index].Enabled;
+        }
+
+
+        public double GetSensorValue(int Index)
+        {
+            return FPhidget.bridges[Index].BridgeValue;
+        }
+
+        public double GetBridgeMax(int Index)
+        {
+            return FPhidget.bridges[Index].BridgeMax;
+        }
+
+        public double GetBridgeMin(int Index)
+        {
+            return FPhidget.bridges[Index].BridgeMin;
+        }
+
+        public string GetDataRateMin(int Index)
+        {
+            return FPhidget.bridges[Index].Gain.ToString();
+        }
+
+
+        #endregion getter functions
+
+
+
+
+        public override void AddChangedHandler()
+        {
+            FPhidget.BridgeData += new BridgeDataEventHandler(SensorChange);
+        }
+
+        void SensorChange(object sender, BridgeDataEventArgs e)
+        {
+            FChanged = true;
+			FSensorChanged = true;
+        }
+
+        public override void RemoveChangedHandler()
+        {
+            FPhidget.BridgeData -= new BridgeDataEventHandler(SensorChange);
+        }
+
+
+        public int Count
+        {
+            get { return FPhidget.bridges.Count; }
+        }
+
+
+        public bool Changed
+        {
+            get
+            {
+                bool temp = FChanged;
+                FChanged = false;
+                return temp;
+            }
+        }
+    }
+}
